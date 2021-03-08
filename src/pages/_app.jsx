f80dd56f5f126f64504ef09195b77753b752cc38
joevtap <joelvitortorres@gmail.com>
2021-03-07 21:39:22 -0300

    Setup completo

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,8 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import React from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Projeto</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>The Flexboxers</h1>
+            </main>
+        </div>
+    );
+}
