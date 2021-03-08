654bdb8a3c01ebff7c205694229935e597342e29
joevtap <joelvitortorres@gmail.com>
2021-03-08 15:22:36 -0300

    Profile pic fixed

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,13 +3,11 @@ import styles from "../styles/components/Profile.module.scss";
 const Profile = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.pic}>
-                {false ? (
-                    <img src="./img/masculino.png" />
-                ) : (
-                    <img src="./img/feminino.png" />
-                )}
-            </div>
+            {false ? (
+                <img src="./img/masculino.png" />
+            ) : (
+                <img src="./img/feminino.png" />
+            )}
             <div className={styles.info}>
                 <p>Usuário</p>
                 <span>Nível 1</span>
