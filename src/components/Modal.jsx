245f394e44d8a84223daefd66b1cd2215d6887b5
joevtap <joelvitortorres@gmail.com>
2021-03-10 07:55:59 -0300

    Update no close button do modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,33 @@
 import styles from "../styles/components/Modal.module.scss";
 
-const Modal = ({ id = "modal", onClose = () => {} }) => {
-  const handleOutsideClick = (e) => {
-    if (e.target.id === id) onClose();
-  };
+const Modal = ({ id = "modal", onClose }) => {
+    const handleOutsideClick = (e) => {
+        if (e.target.id === id) onClose();
+    };
 
-  return (
-    <div className={styles.modalActive} id={id} onClick={handleOutsideClick}>
-      <div className={styles.container}>
-        <h1>Título da missão</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          amet itaque minima odio iusto quae quo, natus reiciendis illum
-          assumenda nostrum deserunt earum enim aliquam asperiores esse
-          architecto voluptate suscipit.
-        </p>
-        <button type="button" onClick={onClose}>
-          X
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className={styles.modalActive}
+            id={id}
+            onClick={handleOutsideClick}
+        >
+            <div className={styles.container}>
+                <h1>Título da missão</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quibusdam amet itaque minima odio iusto quae quo, natus
+                    reiciendis illum assumenda nostrum deserunt earum enim
+                    aliquam asperiores esse architecto voluptate suscipit.
+                </p>
+                <button type="button" onClick={onClose}>
+                    <div className={styles.closeButton}>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Modal;
