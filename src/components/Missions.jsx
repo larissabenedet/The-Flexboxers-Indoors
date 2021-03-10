9627318f35aeb9ddb23792ac1b7aeba46342848e
joevtap <joelvitortorres@gmail.com>
2021-03-10 17:44:20 -0300

    Duas fileiras de missoes

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -4,26 +4,53 @@ import { MissionContext } from "../contexts/MissionsContext";
 import allMissions from "../../missions.json";
 
 const DashboardMenu = () => {
-  const { newMission } = useContext(MissionContext);
-  return (
-    <div className={styles.container}>
-      <div className={styles.missionsTree}>
-        <div>
-          {allMissions.map((mission, i) => (
-            <div className={styles.mission} key={mission[i]}>
-              <img
-                src={mission.img}
-                key={mission[i]}
-                alt={mission.title}
-                onClick={newMission}
-                id={i}
-              />
+    const { newMission } = useContext(MissionContext);
+    return (
+        <div className={styles.container}>
+            <div className={styles.missionsTree}>
+                <div>
+                    {allMissions.map((mission, i) => {
+                        if (i < 3) {
+                            return (
+                                <div
+                                    className={styles.mission}
+                                    key={mission[i]}
+                                >
+                                    <img
+                                        src={mission.img}
+                                        key={mission[i]}
+                                        alt={mission.title}
+                                        onClick={newMission}
+                                        id={i}
+                                    />
+                                </div>
+                            );
+                        }
+                    })}
+                </div>
+                <div>
+                    {allMissions.map((mission, i) => {
+                        if (i >= 3) {
+                            return (
+                                <div
+                                    className={styles.mission}
+                                    key={mission[i]}
+                                >
+                                    <img
+                                        src={mission.img}
+                                        key={mission[i]}
+                                        alt={mission.title}
+                                        onClick={newMission}
+                                        id={i}
+                                    />
+                                </div>
+                            );
+                        }
+                    })}
+                </div>
             </div>
-          ))}
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default DashboardMenu;
