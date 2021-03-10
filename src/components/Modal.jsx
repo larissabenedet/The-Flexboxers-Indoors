25f73b4c131711f85bb92004fb7c1b872d5ad2eb
joevtap <joelvitortorres@gmail.com>
2021-03-10 16:32:55 -0300

    Modal missoes estilizado

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,38 +5,48 @@ const Modal = ({ id = "modal", onClose }) => {
         if (e.target.id === id) onClose();
     };
 
-  return (
-    <div className={styles.modalActive} id={id} onClick={handleOutsideClick}>
-      <div className={styles.container}>
-        <button type="button" onClick={onClose}>
-          <span>X</span>
-        </button>
-        <div className={styles.imgContainer}>
-          <img src="img/bolo.png" alt="mission"/>
-        </div>
-        <div>
-          <h1>Título da missão</h1>          
-          <span>Ganhe: 90xp</span>
-        </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          amet itaque minima odio iusto quae quo, natus reiciendis illum
-          assumenda nostrum deserunt earum enim aliquam asperiores esse
-          architecto voluptate suscipit.
-        </p>
-       
-        <div className={styles.buttons}>
-          <button type="button">
-            Desistir
-          </button>
-          <button type="button">
-            Concluir
-          </button>
-        </div>
+    return (
+        <div
+            className={styles.modalActive}
+            id={id}
+            onClick={handleOutsideClick}
+        >
+            <div className={styles.container}>
+                <button
+                    className={styles.closeButton}
+                    type="button"
+                    onClick={onClose}
+                >
+                    <div></div>
+                    <div></div>
+                </button>
 
-      </div>
-    </div>
-  );
+                <header>
+                    <div className={styles.imgContainer}>
+                        <img src="img/bolo.png" alt="mission" />
+                    </div>
+                    <div>
+                        <h1>Título da missão</h1>
+                        <span>Ganhe: 90xp</span>
+                    </div>
+                </header>
+
+                <main>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quibusdam amet itaque minima odio iusto quae quo, natus
+                        reiciendis illum assumenda nostrum deserunt earum enim
+                        aliquam asperiores esse architecto voluptate suscipit.
+                    </p>
+                </main>
+
+                <footer>
+                    <button type="button">Desistir</button>
+                    <button type="button">Concluir</button>
+                </footer>
+            </div>
+        </div>
+    );
 };
 
 export default Modal;
