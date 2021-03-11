e30394a1bbcc69f742de76bc29da0d85154a3121
joevtap <joelvitortorres@gmail.com>
2021-03-11 18:40:16 -0300

    Estilo do cadastro inciado

--- src/components/ModalUser.jsx
+++ src/components/ModalUser.jsx
@@ -1,71 +1,106 @@
-import styles from "../styles/components/Modal.module.scss";
+import styles from "../styles/components/ModalUser.module.scss";
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext.jsx";
 
 const ModalUser = ({ onClose }) => {
-  const {
-    handleClassChange,
-    handleInputEntry,
-    handleGenderChange,
-  } = useContext(UserContext);
+    const {
+        handleClassChange,
+        handleInputEntry,
+        handleGenderChange,
+    } = useContext(UserContext);
 
-  return (
-    <div className={styles.modalActive}>
-      <div className={styles.container}>
-        <h1>Antes de começar sua aventura, por favor crie seu personagem:</h1>
-        <form>
-          <p>Nickname</p>
-          <div>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              onChange={handleInputEntry}
-            />
-          </div>
+    return (
+        <div className={styles.modalActive}>
+            <div className={styles.container}>
+                <h1>
+                    Antes de começar sua aventura, por favor crie seu personagem
+                </h1>
+                <form>
+                    <div className={styles.nickGenderContainer}>
+                        <div className={styles.nick}>
+                            <p>Nickname</p>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                onChange={handleInputEntry}
+                            />
+                        </div>
 
-          <p>Gênero</p>
-          <div onChange={handleGenderChange}>
-            <input
-              type="radio"
-              id="masculino"
-              name="genero"
-              value="Masculino"
-            />
-            <label htmlFor="masculino"> Masculino</label>
-            <input type="radio" id="feminino" name="genero" value="Feminino" />
-            <label htmlFor="feminino"> Feminino</label>
-          </div>
-
-          <p>Classe</p>
-          <div onChange={handleClassChange}>
-            <input type="radio" id="artista" name="classe" value="Artista" />
-            <label htmlFor="artista"> Artista</label>
-            <input
-              type="radio"
-              id="estudioso"
-              name="classe"
-              value="Estudioso"
-            />
-            <label htmlFor="estudioso"> Estudioso</label>
-            <input
-              type="radio"
-              id="explorador"
-              name="classe"
-              value="Explorador"
-            />
-            <label htmlFor="explorador"> Explorador</label>
-          </div>
-
-          <footer>
-            <button type="submit" onClick={onClose}>
-              Concluído
-            </button>
-          </footer>
-        </form>
-      </div>
-    </div>
-  );
+                        <div
+                            onChange={handleGenderChange}
+                            className={styles.gender}
+                        >
+                            <p>Gênero</p>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="masculino"
+                                    name="genero"
+                                    value="Masculino"
+                                />
+                                <label htmlFor="masculino"> Masculino</label>
+                            </div>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="feminino"
+                                    name="genero"
+                                    value="Feminino"
+                                />
+                                <label htmlFor="feminino"> Feminino</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.classesContainer}>
+                        <p>Classe</p>
+                        <div
+                            onChange={handleClassChange}
+                            className={styles.classes}
+                        >
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="artista"
+                                    name="classe"
+                                    value="Artista"
+                                />
+                                <label htmlFor="artista"></label>
+                                <p>Artista</p>
+                            </div>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="estudioso"
+                                    name="classe"
+                                    value="Estudioso"
+                                />
+                                <label htmlFor="estudioso"></label>
+                                <p>Estudioso(a)</p>
+                            </div>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="explorador"
+                                    name="classe"
+                                    value="Explorador"
+                                />
+                                <label htmlFor="explorador"></label>
+                                <p>Explorador(a)</p>
+                            </div>
+                        </div>
+                        <button
+                            className={styles.completeButton}
+                            type="submit"
+                            onClick={onClose}
+                        >
+                            Concluído
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default ModalUser;
