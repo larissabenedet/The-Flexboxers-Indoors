1ae004611c4d9929755c15416fb7b1f1bcea6438
joevtap <joelvitortorres@gmail.com>
2021-03-13 17:04:41 -0300

    Home atualizada

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,34 +11,37 @@ export default function Home() {
             <Head>
                 <title>Indoors</title>
             </Head>
-            <div className={styles.logo}>
-                <h1>
-                    <img src="./img/house.png" alt="Fique em casa" />
-                    Indoors
-                </h1>
-                <h2>Uma aventura em casa</h2>
-            </div>
-            <main>
-                <div className={styles.left}>
-                    <Link href="/dashboard" passHref>
-                        <a className={styles.playButton}>
-                            <svg
-                                version="1.1"
-                                width="200"
-                                height="200"
-                                viewBox="0 0 512 512"
-                            >
-                                <g></g>
-                                <path d="M152.443 136.417l207.114 119.573-207.114 119.593z" />
-                            </svg>
-                        </a>
-                    </Link>
-                </div>
 
-                <div className={styles.right}>
-                    <Link href="/sobre" passHref>
-                        <a className={styles.sobre}>Sobre</a>
-                    </Link>
+            <main>
+                <div className={styles.logo}>
+                    <h1>
+                        <img src="./img/house.png" alt="Fique em casa" />
+                        Indoors
+                    </h1>
+                    <h2>Uma aventura em casa</h2>
+                </div>
+                <div className={styles.linksContainer}>
+                    <div className={styles.left}>
+                        <Link href="/dashboard" passHref>
+                            <a className={styles.playButton}>
+                                <svg
+                                    version="1.1"
+                                    width="200"
+                                    height="200"
+                                    viewBox="0 0 512 512"
+                                >
+                                    <g></g>
+                                    <path d="M152.443 136.417l207.114 119.573-207.114 119.593z" />
+                                </svg>
+                            </a>
+                        </Link>
+                    </div>
+
+                    <div className={styles.right}>
+                        <Link href="/sobre" passHref>
+                            <a className={styles.sobre}>Sobre</a>
+                        </Link>
+                    </div>
                 </div>
             </main>
         </div>
