59ed3b3d2cf672ecb7b7fbc7303db512fd140b43
joevtap <joelvitortorres@gmail.com>
2021-03-13 09:36:38 -0300

    Começo do desenvolvimento da home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,37 @@
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/pages/Home.module.scss";
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps } from "next";
 import { UserContext } from "../contexts/UserContext";
 import { MissionContext } from "../contexts/MissionsContext";
 
 export default function Home() {
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
                 <title>Indoors</title>
             </Head>
-            <h1>Indoors</h1>
+            <div className={styles.logo}>
+                <h1>
+                    <img src="./img/house.png" alt="Fique em casa" />
+                    Indoors
+                </h1>
+                <h2>Uma aventura em casa</h2>
+            </div>
             <main>
                 <div className={styles.left}>
                     <Link href="/dashboard" passHref>
-                        <a className={styles.playButton}>Começar</a>
+                        <a className={styles.playButton}>
+                            <svg
+                                version="1.1"
+                                width="200"
+                                height="200"
+                                viewBox="0 0 512 512"
+                            >
+                                <g></g>
+                                <path d="M152.443 136.417l207.114 119.573-207.114 119.593z" />
+                            </svg>
+                        </a>
                     </Link>
                 </div>
 
@@ -28,4 +44,3 @@ export default function Home() {
         </div>
     );
 }
-
